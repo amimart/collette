acb70ea3b107fd82500e48fbe182a1b1307b35b4
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2026-07-08 11:49:31 +0200

    feat: limit backend error constructor cfg

--- src/error.rs
+++ src/error.rs
@@ -25,6 +25,7 @@ pub enum Error {
 pub struct BackendError(Box<dyn std::error::Error + Send + Sync>);
 
 impl BackendError {
+    #[cfg(any(test, feature = "redb"))]
     pub(crate) fn new(e: impl std::error::Error + Send + Sync + 'static) -> Self {
         BackendError(Box::new(e))
     }
