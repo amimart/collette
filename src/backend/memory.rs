da170a5e6f007916a4a255cb4f8323fa7edd13a7
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2026-07-16 09:04:08 +0200

    refactor: adapt memory multistore errors

--- src/backend/memory.rs
+++ src/backend/memory.rs
@@ -4,13 +4,13 @@
 //! Reads see stable snapshots, while writes are staged and published atomically
 //! on commit.
 
-use crate::error::BackendError;
 use crate::scan::Direction;
 use crate::store::{
     MultiStore, MultiStoreReadHandle, MultiStoreWriteHandle, ReadKVStore, ReadWriteKVStore,
     WriteKVStore,
 };
 use std::collections::BTreeMap;
+use std::convert::Infallible;
 use std::ops::RangeBounds;
 use std::sync::{Arc, Condvar, Mutex, RwLock};
 use std::vec::IntoIter;
@@ -35,7 +35,7 @@ pub type KVStore = BTreeMap<Vec<u8>, Vec<u8>>;
 #[doc(hidden)]
 pub type ScanItem = (Vec<u8>, Vec<u8>);
 #[doc(hidden)]
-pub type ScanResult = Result<ScanItem, BackendError>;
+pub type ScanResult = Result<ScanItem, Infallible>;
 #[doc(hidden)]
 pub type ScanResults = Vec<ScanResult>;
 
@@ -106,6 +106,7 @@ impl InMemoryMultiStore {
 }
 
 impl MultiStore for InMemoryMultiStore {
+    type Error = Infallible;
     type ReadHandle = InMemoryReadHandle;
     type WriteHandle = InMemoryWriteHandle;
 
@@ -113,7 +114,7 @@ impl MultiStore for InMemoryMultiStore {
         &self,
         namespace: &'static str,
         stores: impl IntoIterator<Item = &'static str>,
-    ) -> Result<(), BackendError> {
+    ) -> Result<(), Self::Error> {
         let stores = stores.into_iter().collect::<Vec<_>>();
         let mut db = self.stores.write().unwrap();
 
@@ -143,7 +144,7 @@ impl MultiStore for InMemoryMultiStore {
         Ok(())
     }
 
-    fn read(&self, namespace: &'static str) -> Result<Self::ReadHandle, BackendError> {
+    fn read(&self, namespace: &'static str) -> Result<Self::ReadHandle, Self::Error> {
         let db = self.stores.read().unwrap();
         let state = db.get(namespace).unwrap();
         let snapshot = state.stores.read().unwrap().clone();
@@ -151,7 +152,7 @@ impl MultiStore for InMemoryMultiStore {
         Ok(InMemoryReadHandle { stores: snapshot })
     }
 
-    fn write(&self, namespace: &'static str) -> Result<Self::WriteHandle, BackendError> {
+    fn write(&self, namespace: &'static str) -> Result<Self::WriteHandle, Self::Error> {
         let db = self.stores.read().unwrap();
         let state = db.get(namespace).unwrap();
         let permit = state.writer.acquire();
@@ -175,9 +176,10 @@ pub struct InMemoryReadHandle {
 }
 
 impl MultiStoreReadHandle for InMemoryReadHandle {
+    type Error = Infallible;
     type Store = InMemoryReadStore;
 
-    fn open_store(&self, name: &'static str) -> Result<Self::Store, BackendError> {
+    fn open_store(&self, name: &'static str) -> Result<Self::Store, Self::Error> {
         Ok(InMemoryReadStore {
             store: self.stores.get(name).unwrap().clone(),
         })
@@ -190,9 +192,10 @@ pub struct InMemoryReadStore {
 }
 
 impl ReadKVStore for InMemoryReadStore {
+    type Error = Infallible;
     type Iter = IntoIter<ScanResult>;
 
-    fn get(&self, key: impl AsRef<[u8]>) -> Result<Option<Vec<u8>>, BackendError> {
+    fn get(&self, key: impl AsRef<[u8]>) -> Result<Option<Vec<u8>>, Self::Error> {
         Ok(self.store.get(key.as_ref()).cloned())
     }
 
@@ -200,7 +203,7 @@ impl ReadKVStore for InMemoryReadStore {
         self,
         range: impl RangeBounds<Vec<u8>>,
         direction: Direction,
-    ) -> Result<Self::Iter, BackendError> {
+    ) -> Result<Self::Iter, Self::Error> {
         let scan: ScanResults = match direction {
             Direction::LeftToRight => self
                 .store
@@ -227,15 +230,16 @@ pub struct InMemoryWriteHandle {
 }
 
 impl MultiStoreWriteHandle for InMemoryWriteHandle {
+    type Error = Infallible;
     type Store<'a> = InMemoryWriteStore<'a>;
 
-    fn open_store(&mut self, name: &'static str) -> Result<Self::Store<'_>, BackendError> {
+    fn open_store(&mut self, name: &'static str) -> Result<Self::Store<'_>, Self::Error> {
         Ok(InMemoryWriteStore {
             store: self.staged.get_mut(name).unwrap(),
         })
     }
 
-    fn commit(self) -> Result<(), BackendError> {
+    fn commit(self) -> Result<(), Self::Error> {
         let new_stores = self
             .staged
             .into_iter()
@@ -253,25 +257,30 @@ pub struct InMemoryWriteStore<'a> {
     store: &'a mut KVStore,
 }
 
-impl<'a> ReadWriteKVStore<'a> for InMemoryWriteStore<'a> {}
+impl<'a> ReadWriteKVStore<'a> for InMemoryWriteStore<'a> {
+    type Error = Infallible;
+}
 
 impl<'a> WriteKVStore<'a> for InMemoryWriteStore<'a> {
-    fn set(&mut self, key: impl AsRef<[u8]>, value: impl AsRef<[u8]>) -> Result<(), BackendError> {
+    type Error = Infallible;
+
+    fn set(&mut self, key: impl AsRef<[u8]>, value: impl AsRef<[u8]>) -> Result<(), Self::Error> {
         self.store
             .insert(key.as_ref().to_owned(), value.as_ref().to_owned());
         Ok(())
     }
 
-    fn remove(&mut self, key: impl AsRef<[u8]>) -> Result<(), BackendError> {
+    fn remove(&mut self, key: impl AsRef<[u8]>) -> Result<(), Self::Error> {
         self.store.remove(&key.as_ref().to_owned());
         Ok(())
     }
 }
 
 impl ReadKVStore for InMemoryWriteStore<'_> {
+    type Error = Infallible;
     type Iter = IntoIter<ScanResult>;
 
-    fn get(&self, key: impl AsRef<[u8]>) -> Result<Option<Vec<u8>>, BackendError> {
+    fn get(&self, key: impl AsRef<[u8]>) -> Result<Option<Vec<u8>>, Self::Error> {
         Ok(self.store.get(key.as_ref()).map(|v| v.to_owned()))
     }
 
@@ -279,7 +288,7 @@ impl ReadKVStore for InMemoryWriteStore<'_> {
         self,
         range: impl RangeBounds<Vec<u8>>,
         direction: Direction,
-    ) -> Result<Self::Iter, BackendError> {
+    ) -> Result<Self::Iter, Self::Error> {
         let scan: ScanResults = match direction {
             Direction::LeftToRight => self
                 .store
