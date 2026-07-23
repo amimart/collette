2dc73c5116f86e6b76834abe27aa4eeb69a5fb1f
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2026-07-23 11:34:10 +0200

    feat: add scan chaining from directed scan

--- src/scan.rs
+++ src/scan.rs
@@ -282,6 +282,65 @@ where
     }
 }
 
+impl<'a, S, R> RangeScan<'a, S, R>
+where
+    S: Scan,
+    R: RangeBounds<S::Key<'a>>,
+{
+    pub fn direction(self, direction: Direction) -> DirectedScan<'a, Self> {
+        DirectedScan {
+            direction,
+            inner: self,
+            _marker: Default::default(),
+        }
+    }
+
+    pub fn after(self, cursor: Vec<u8>) -> AfterScan<'a, Self> {
+        AfterScan {
+            cursor,
+            inner: self,
+            _marker: Default::default(),
+        }
+    }
+}
+
+pub struct DirectedScan<'a, S> {
+    direction: Direction,
+    inner: S,
+
+    _marker: PhantomData<&'a ()>,
+}
+
+impl<'a, S> Scan for DirectedScan<'a, S>
+where
+    S: Scan,
+{
+    type Key<'b> = S::Key<'b>
+    where
+        Self: 'b;
+    type Executor = S::Executor;
+
+    fn compile(self) -> Result<CompiledScan<Self::Executor>, Error> {
+        let mut scan = self.inner.compile()?;
+        scan.direction = self.direction;
+
+        Ok(scan)
+    }
+}
+
+impl<'a, S> DirectedScan<'a, S>
+where
+    S: Scan,
+{
+    pub fn after(self, cursor: Vec<u8>) -> AfterScan<'a, Self> {
+        AfterScan {
+            cursor,
+            inner: self,
+            _marker: Default::default(),
+        }
+    }
+}
+
 pub struct AfterScan<'a, S>
 where
     S: Scan + 'a,
@@ -312,30 +371,6 @@ where
             Direction::LeftToRight => scan.range.0 = Bound::Excluded(self.cursor),
             Direction::RightToLeft => scan.range.1 = Bound::Excluded(self.cursor),
         }
-
-        Ok(scan)
-    }
-}
-
-pub struct DirectedScan<'a, S> {
-    direction: Direction,
-    inner: S,
-
-    _marker: PhantomData<&'a ()>,
-}
-
-impl<'a, S> Scan for DirectedScan<'a, S>
-where
-    S: Scan,
-{
-    type Key<'b> = S::Key<'b>
-    where
-        Self: 'b;
-    type Executor = S::Executor;
-
-    fn compile(self) -> Result<CompiledScan<Self::Executor>, Error> {
-        let mut scan = self.inner.compile()?;
-        scan.direction = self.direction;
 
         Ok(scan)
     }
